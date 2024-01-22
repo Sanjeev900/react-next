a7f407a11a29a03458c83a68a1717cef1b4b6de6
Sanjeev Kumar <sk330511@gmail.com>
2024-01-22 16:17:22 +0530

    get username using getServerSideProps in next app

--- xperium-next-app/pages/user-profile.js
+++ xperium-next-app/pages/user-profile.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 
-const UserProfile = () => {
-
-  const router = useRouter();
-  const receivedUsername = router.query.data || 'User';
-
+const UserProfile = ({ user }) => {
   return (
     <div>
       <h1>User Profile Page (Next.js)</h1>
       <br />
-      <h3>Hi {receivedUsername}</h3>
+      <h3>Hi {user.name}</h3>
     </div>
-
   );
+};
+
+export const getServerSideProps = async ({ query }) => {
+  const user = {
+    name: decodeURIComponent(query.data || 'User'),
+  };
+
+  return {
+    props: { user },
+  };
 };
 
 export default UserProfile;
